7a00fb7fdd380580d0ab4827c4416f218bc5b287
minwoo <mark42son@gmail.com>
2023-11-15 09:19:20 +0900

    codetree 3rd review

---
+++ CodeTree/최장 연속 부분 수열/T를 초과하는 연속 부분 수열(RE).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_N 1000
+
+using namespace std;
+
+
+int n;
+int t;
+vector<int> arr(n);
+int main() {
+
+    cin >> n >> t;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int maxLen = 0;
+    int currentLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > t)
+            currentLen++;
+        else
+            currentLen = 0;
+
+        maxLen = max(maxLen, currentLen);
+    }
+
+    cout << maxLen;
+    return 0;
+}
+//두 번째 줄에는 n개의 수가 공백을 사이에 두고 주어집니다.
+//
+//t보다 큰 수로만 이루어진 연속 부분 수열 중 최대 길이를 구하는 프로그램을 작성
+
+
