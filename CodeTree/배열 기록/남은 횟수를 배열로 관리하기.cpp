f474d06d59c84d76d389ba15a11ac848bccc39a7
minwoo <mark42son@gmail.com>
2023-11-15 13:43:15 +0900

    5th cpp array

---
+++ CodeTree/배열 기록/남은 횟수를 배열로 관리하기.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Mac Apple on 2023/11/15.
+//
+
+#include "남은 횟수를 배열로 관리하기.h"
+
+#include <iostream>
+
+int main() {
+
+    int target_nums[3] = {5, 10, 8};
+    int candy_nums[3] = {3, 3, 3};
+    int queries_nums[5] = {'fq', 'sq', ' ',' ',' '};
+
+    for (int query = 0; query < 5; query++)
+        for (int num = 0; num < 3; num++)
+            if(MeetTheCondition(query, num))
+                candy_nums[num]--;
+    return 0;
+}
