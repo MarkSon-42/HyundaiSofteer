cafa14bac185ef8eb294cade684d33379b2e9199
minwoo <mark42son@gmail.com>
2023-11-15 13:37:23 +0900

    cmake init

---
+++ build/CMakeFiles/HyundaiSofteer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/macapple/Documents/Codes/HyundaiSofteer/CodeTree/배열 기록/만나는 그 순간.cpp" "CMakeFiles/HyundaiSofteer.dir/CodeTree/배열_기록/만나는_그_순간.cpp.o" "gcc" "CMakeFiles/HyundaiSofteer.dir/CodeTree/배열_기록/만나는_그_순간.cpp.o.d"
+  "/Users/macapple/Documents/Codes/HyundaiSofteer/CodeTree/최장 연속 부분 수열/T를 초과하는 연속 부분 수열(RE).cpp" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열(RE).cpp.o" "gcc" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열(RE).cpp.o.d"
+  "/Users/macapple/Documents/Codes/HyundaiSofteer/CodeTree/최장 연속 부분 수열/T를 초과하는 연속 부분 수열.cpp" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열.cpp.o" "gcc" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열.cpp.o.d"
+  "/Users/macapple/Documents/Codes/HyundaiSofteer/CodeTree/최장 연속 부분 수열/연속되는 수 4.cpp" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/연속되는_수_4.cpp.o" "gcc" "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/연속되는_수_4.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HyundaiSofteer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/배열_기록/만나는_그_순간.cpp.o"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/배열_기록/만나는_그_순간.cpp.o.d"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열(RE).cpp.o"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열(RE).cpp.o.d"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열.cpp.o"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/T를_초과하는_연속_부분_수열.cpp.o.d"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/연속되는_수_4.cpp.o"
+  "CMakeFiles/HyundaiSofteer.dir/CodeTree/최장_연속_부분_수열/연속되는_수_4.cpp.o.d"
+  "HyundaiSofteer"
+  "HyundaiSofteer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HyundaiSofteer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
